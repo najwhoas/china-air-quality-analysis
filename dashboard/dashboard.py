24855f98f45c36555d8a63fb16b2c7613a20947f
Najwas <124247939+najwhoas@users.noreply.github.com>
2024-03-07 08:44:08 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,155 +1,113 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('agg')
+import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
-# -*- coding: utf-8 -*-
 
+all_df = pd.read_csv("https://raw.githubusercontent.com/najwhoas/china-air-quality-analysis/main/dashboard/all_data.csv")
+df = pd.read_csv("https://github.com/marceloreis/HTI/raw/master/PRSA_Data_20130301-20170228/PRSA_Data_Gucheng_20130301-20170228.csv")
 
-st.header('Proyek Analisis Data: Air Quality Analysis')
-"""- **Nama:** Najwa Salsabila
-- **Email:** najwasalsabieee@gmail.com
-- **ID Dicoding:** najwasalsabila"""
-
-
-df = pd.read_csv('https://raw.githubusercontent.com/najwhoas/china-air-quality-analysis/main/dashboard/all_data.csv')
-
-
-st.subheader('DataFrame Overview')
-df_desc = df.describe().transpose()
-st.write(df_desc)
-
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-st.write(" ")
-
-st.subheader('Polutan apa saja yang berhubungan dengan jumlah penyebaran Partikulat 2.5?')
-plt.figure(figsize=(20,7))
-sns.heatmap(df.corr(numeric_only=True), vmin=-1, vmax=1, center=0, cmap='YlGnBu', annot=True)
-st.pyplot(plt)
-st.write('Polutan paling signifikan hubungannya dengan Partikulat 2.5 adalah Partikulat 10, lalu disusul dengan Karbon Monoksida, Nitrogen Dioksida, Sulfur Dioksida, dan titik embun.')
-
-st.subheader('Bagaimana perbandingan rata-rata sebaran Partikulat 2.5 dan Partikulat 10 pada setiap wilayah?')
-partikulats = df.groupby('Wilayah')[['Partikulat_2.5','Partikulat_10',]].mean().reset_index()
-partikulats
-
-c=np.arange(len(partikulats['Wilayah']))
-plt.figure(figsize=(16,7))
-plt.xticks(c,partikulats['Wilayah'])
-plt.xlabel('Wilayah')
-plt.ylabel('Persebaran Partikulat')
-plt.bar(c,partikulats['Partikulat_10'],label='Partikulat 10',width=0.4,color='#8CB9BD')
-plt.bar(c+0.2,partikulats['Partikulat_2.5'],label='Partikulat 2.5',width=0.4,color='#ECB159')
-plt.legend()
-plt.title("Rata-rata Penyebaran Partikulat di Tiap Wilayah", fontsize=15)
-plt.grid(axis='y')
-st.pyplot(plt)
-
-c=np.arange(len(partikulats['Wilayah']))
-colorspm25 = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#ECB159", "#D3D3D3","#D3D3D3", "#D3D3D3", "#D3D3D3","#D3D3D3", "#D3D3D3", "#D3D3D3","#D3D3D3"]
-colorspm10 = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#8CB9BD", "#D3D3D3", "#D3D3D3","#D3D3D3", "#D3D3D3", "#D3D3D3","#D3D3D3"]
-plt.figure(figsize=(16,7))
-plt.xticks(c,partikulats['Wilayah'])
-plt.xlabel('Wilayah')
-plt.ylabel('Persebaran Partikulat')
-plt.bar(c,partikulats['Partikulat_10'],width=0.4,color=colorspm10)
-plt.bar(c+0.2,partikulats['Partikulat_2.5'],width=0.4,color=colorspm25)
-plt.title("Rata-rata Penyebaran Partikulat Terbesar", fontsize=15)
-plt.grid(axis='y')
-st.pyplot(plt)
-st.write('Untuk setiap wilayah, kadar polutan PM 10 selalu lebih tinggi dari PM 2.5. Penyebaran polutan PM 10 terbanyak terjadi di wilayah Gucheng. Sementara, untuk penyebaran polutan PM 2.5 paling banyak terjadi di Dongsi, walau sebenarnya hanya berselisih dengan Gucheng sebesar 1.52 ppm. Dengan angka ini, dapat dikatakan bahwa wilayah dengan kadar polutan PM 2.5 dan PM 10 yang penyebarannya tinggi adalah Gucheng.')
-
-st.write(" ")
-
-st.subheader('Bagaimana proporsi polutan Sulfur Dioksida, Nitrogen Dioksida, dan Karbon Monoksida pada Tahun 2017 di Wilayah Gucheng?')
-
-gucheng_df = pd.read_csv("https://github.com/marceloreis/HTI/raw/master/PRSA_Data_20130301-20170228/PRSA_Data_Gucheng_20130301-20170228.csv")
-
-gucheng_df.fillna(method='ffill', inplace=True, axis=0)
-
-numeric_df = gucheng_df.select_dtypes(include='number')
-
+df.fillna(method='ffill', inplace=True, axis=0)
+numeric_df = df.select_dtypes(include='number')
 Q1 = numeric_df.quantile(0.25)
 Q3 = numeric_df.quantile(0.75)
 IQR = Q3 - Q1
-
 maximum = Q3 + (1.5 * IQR)
 minimum = Q1 - (1.5 * IQR)
-
 kondisi_lower_than = numeric_df < minimum
 kondisi_more_than = numeric_df > maximum
-gucheng_df = gucheng_df.mask(cond=kondisi_more_than, other=maximum, axis=0)
-gucheng_df = gucheng_df.mask(cond=kondisi_lower_than, other=minimum, axis=0)
+df = df.mask(cond=kondisi_more_than, other=maximum, axis=0)
+df = df.mask(cond=kondisi_lower_than, other=minimum, axis=0)
+
+st.title('Gucheng Air Quality 🌨️')
+st.write('Analysis of air quality in Gucheng, China&mdash;from 2013 to 2017.')
+st.write('*&mdash;by: Najwa Salsabila*')
+
+st.write("__________________")
+
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+        .container {
+            padding: 10px;
+            border: 1px solid #d3d3d3;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+        .tabs {
+            # font-size: 100px !important;
+            }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.header('Recent Condition  &mdash;•')
+container = st.container()
+columns = st.columns(3)
 
 
+tile1 = columns[0].container(height=130)
+tile1.metric("Average CO Concentration", "82.3%")
+tile2 = columns[1].container(height=130)
+tile2.metric("Average PM 2.5", "5.03%")
+tile3 = columns[2].container(height=130)
+tile3.metric("Temperature", "2.2°C")
 
-gucheng_df.info()
+st.header('')
+st.header('')
 
-mean = gucheng_df[gucheng_df['year'] == 2017][['PM2.5','PM10','SO2', 'NO2', 'CO']].mean()
+tab1,tab2,tab3,tab4,tab5,tab6,tab7 = st.tabs(["Data", "Correlation", "Polutant Proportions", "Particulat Consentration", "Polutant Trend", "Ozon Trend","Insight"])
+
+
+tab1.subheader('DataFrame Overview &mdash;')
+df_desc = all_df.describe().transpose()
+tab1.write(df_desc)
+
+tab2.subheader('Columns Correlation &mdash;')
+fig = px.imshow(all_df.corr(), color_continuous_scale='YlGnBu')
+tab2.plotly_chart(fig)
+tab2.write('The most significant pollutant correlated with Particulate 2.5 is Particulate 10, followed by Carbon Monoxide, Nitrogen Dioxide, Sulfur Dioxide, and dew point.')
+
+tab3.subheader('Pollutants Proportions in 2017 &mdash;')
+polutant = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO']
+mean = df[df['year'] == 2017][['PM2.5', 'PM10', 'SO2', 'NO2', 'CO']].mean()
 total = mean.sum()
+proportion = mean / total
+col1, col2 = st.columns([1, 3])
+fig = px.pie(names=polutant, values=proportion.values, labels=proportion.index,
+             color_discrete_sequence=px.colors.sequential.YlGnBu_r)
+tab3.plotly_chart(fig)
+tab3.write('In 2017, of the top 100% of pollutants in Gucheng, 85.3% was dominated by carbon monoxide. This was followed by the proportion of PM 10 at 6%, PM 2.5 at 4.8%, Nitrogen Dioxide at 2.9%, and Sulfur Dioxide at 1%. This indicates that pollutants in the form of exhaust gases from vehicles or other kinds of burning equipment dominate the air pollution in Gucheng.')
 
 
-fig, ax = plt.subplots()
-ax.pie(mean/total, autopct='%0.1f%%', colors=['#F39233','#EEF296','#EF4F4F','#9ADE7B','#2D9596'], startangle=140)
-ax.axis('equal')
-ax.legend(['PM2.5','PM10','SO2', 'NO2', 'CO'])
-ax.set_title('Proporsi Polutan Udara di Wilayah Gucheng Tahun 2017')
-st.pyplot(fig)
-st.write('Pada tahun 2017, dari 100% besar polutan yang ada di Gucheng, 85.3% didominasi oleh karbon monoksida. Kemudian disusul dengan proporsi PM 10 sebesar 6%, PM 2.5 sebesar 4.8%, Nitrogen Dioksida sebesar 2.9%, dan Sulfur Dioksida 1%. Ini mengindikasikan bahwa polutan berupa gas buang dari kendaraan atau macam-macam alat pembakaran lainnya mendominasi polusi udara di Gucheng.')
-
-st.write(" ")
-
-st.subheader('Bagaimana tren persebaran polutan Partikulat 2.5, Partikulat 10, Sulfur Dioksida, Nitrogen Dioksida, dan Karbon Monoksida per 6 bulan di wilayah Gucheng?')
-
-gucheng_df['waktu'] = df['Waktu_Pengamatan']
-gucheng_df['waktu'] = pd.to_datetime(gucheng_df['waktu'])
-
-gucheng_df.set_index('waktu', inplace=True)
-
-data = gucheng_df.resample('M').mean()
-
-plt.figure(figsize=(15, 6))
-plt.plot(data['PM2.5'], label='Partikulat 2.5', color='#F39233')
-plt.plot(data['PM10'], label='Partikulat 10', color='#EEF296')
-plt.plot(data['SO2'], label='Sulfur Dioksida', color='#EF4F4F')
-plt.plot(data['NO2'], label='Nitrogen Dioksida', color='#9ADE7B')
-plt.plot(data['CO'], label='Karbon Dioksida', color='#2D9596')
-
-plt.title('Tren Persebaran Polutan per 6 Bulan di Wilayah Gucheng')
-plt.xlabel('Waktu Pengamatan')
-plt.ylabel('Kadar Polutan')
-plt.legend()
-
-st.pyplot(plt)
-st.write('Tren persebaran polutan per 6 bulan di Gucheng menunjukkan bahwa persebaran karbon monoksida selalu mengalami kenaikan yang pesat pada awal tahun. Sementara polutan lainnya cenderung stagnan, tidak terlalu signifikan kenaikan dan penurunannya.')
-
-st.write(" ")
-
-st.subheader("Bagaimana tren keadaan gas ozon dan temperatur udara per 6 Bulan di wilayah Gucheng?")
-
-data = gucheng_df.resample('M').mean()
-
-plt.figure(figsize=(15, 6))
-plt.plot(data['O3'], label='Gas Ozon', color='#F39233')
-plt.plot(data['TEMP'], label='Temperatur Udara', color='#2D9596')
-
-plt.xlabel("Waktu Pengamatan")
-plt.ylabel("Kadar")
-
-plt.title("Tren Keadaan Gas Ozon dan Temperatur Udara per 6 Bulan di Wilayah Gucheng")
-
-plt.legend()
-st.pyplot(plt)
-st.write('Berbanding dengan kondisi karbon monoksida, gas ozon selalu mencapai titik tertingginya di pertengahan tahun (Juli). Keadaan temperatur udara juga mengalami pergerakan naik turun yang sama dengan gas ozon. Dengan keadaan ini, dapat diasumsikan bahwa jika temperatur udara sedang tinggi, besar kemungkinan kadar gas O3 juga tinggi. Ini bisa menjadi petunjuk bagi warga Gucheng untuk berantisipasi melindungi diri, seperti menggunakan masker.')
+tab4.subheader('Average Particulate Concentration in Each Station')
+tab4.header("")
+partikulats = all_df.groupby('Wilayah')[['Partikulat_2.5', 'Partikulat_10']].mean().reset_index()
+tab4.bar_chart(partikulats.set_index('Wilayah'))
+tab4.write('For each station, PM 10 pollutant levels are always higher than PM 2.5. The spread of PM 10 pollutants is highest in the Gucheng station. Meanwhile, the spread of PM 2.5 pollutants is highest in Dongsi, although it is actually only at odds with Gucheng by 1.52 ppm. With these figures, it can be said that the station with the highest levels of PM 2.5 and PM 10 pollutants is Gucheng.')
 
 
-st.header("Insight")
-st.write('Berdasarkan penelitian, Karbon Monoksida (CO) merupakan salah satu prekursor pembentuk gas O3 yang dihasilkan dari proses fotokimia. Berdasarkan data yang ada, keadaan CO dan O3 mengalami pergerakan berlawanan yang lumayan konsisten. Saat CO berada di titik tinggi, O3 berada di titik rendah.')
-st.write('Dengan keadaan tersebut, dapat diasumsikan bahwa proses fotokimia pembentukan ozon permukaan (O3) terjadi selama kurang lebih 6 bulan secara kontinu.')
+df['waktu'] = all_df['Waktu_Pengamatan']
+df['waktu'] = pd.to_datetime(df['waktu'])
+df.set_index('waktu', inplace=True)
+data = df.resample('M').mean()
+
+tab5.subheader('Trend of Polutant Concentration &mdash;')
+tab5.subheader("")
+tab5.line_chart(data[['PM2.5', 'PM10', 'SO2', 'NO2', 'CO']], use_container_width=True)
+tab5.write('The trend of pollutant distribution per 6 months in Gucheng shows that the distribution of carbon monoxide always increases rapidly at the beginning of the year. While other pollutants tend to stagnate, there is not too significant an increase and decrease.')
 
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+tab6.subheader('Trend of Ozon Consentration and Temperature &mdash;')
+tab5.subheader("")
+tab6.line_chart(data[['O3', 'TEMP']], use_container_width=True)
+tab6.write('Compared to carbon monoxide, ozone gas always reaches its highest point in the middle of the year (July). The air temperature also experiences the same up and down movement as ozone gas. With this situation, it can be assumed that if the air temperature is high, it is likely that the O3 gas level is also high. This could be a warning for Gucheng residents to take precautions to protect themselves, such as wearing masks.')
+
+tab7.subheader("Insight &mdash;")
+tab7.subheader("")
+tab7.write('Based on research, Carbon Monoxide (CO) is one of the precursors of O3 gas resulting from photochemical processes. Based on the available data, the state of CO and O3 experiences a fairly consistent opposite movement. When CO is at a high point, O3 is at a low point.')
+tab7.write('Under these circumstances, it can be assumed that the photochemical process of surface ozone (O3) formation occurred for approximately 6 months continuously.')
